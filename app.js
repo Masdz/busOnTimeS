8ceb5bb3526cbc5a247b8bf3def7694cc00f7101
Masdz <masdz1234567@gmail.com>
2017-11-12 22:45:38 -0600

    2
    agregar funcion de eliminar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict"
 var sys = require('util');
 var url = require('url');
-var qs  = require("querystring");
+var qs = require("querystring");
 var HashMap = require('hashmap');
 var http = require('http');
 var querystring = require('querystring');
@@ -11,69 +11,91 @@ var bodyParser = require('body-parser');
 var Schema
 
 var map    = new HashMap();
-var app    = express();
-var cache  = false;
-var best   = 'empty';
+var tmap = new HashMap();
+var app = express();
+var cache = false;
+var best = 'empty';
 var IniDif = 'empty';
-var n1,n2,suma;
+var n1, n2, suma;
 
 map.clear();
 //conexionBD.connect('mongodb://localhost:27017/BusOnTime',(error,respuesta)=>{
 //	if(error){
-	//	throw error;
-	//}else{
+//	throw error;
+//}else{
 //		console.log("Conectado a la BD");
-	//}
-	
+//}
+
 //});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+setInterval(function () {
+    var val = tmap.values();
+    var key = tmap.keys();
+    console.log("Iniciando validacion");
+    for (var i = 0; i < tmap.size; i++) {
+        console.log("Validando "+i+"/"+tmap.size);
+        if(val[i]==='1'){
+            tmap.set(key[i],'0');
+            console.log("un elemento podria ser eliminado");
+        }else if(val[i]==='0'){
+            map.delete(key[i]);
+            tmap.delete(key[i]);
+            console.log("se elimino un elemento");
+        }else{
+            console.log("prro kha");
+        }
+    }
+    console.log("validacion finalizada");
+//},1200000);
+}, 3000);
 
-app.get('/OBTENERCORDENADAS/:id?',function(req,res,next){
-	var response="";
-      if(req.params.id){
-        response=map.get(req.params.id);
-      }
-      else{
-        response = "NO_PARAMS"
-      }
-      res.status(200).send(response);
-  });
-  
-  app.get('/OBTENERCORDENADAS2',function(req,res,next){
-	var response="";
-	var val=map.values();
-	var key=map.keys();
-		for (var i=0;i<map.size;i++){
-			response=response+'"'+key[i]+'":{'+val[i]+'}';
-			if(i+1<map.size){
-				response=response+',';
-			}
-		} 
-    res.status(200).send('{'+response+'}');
-  });
-
-  app.get('/PROBAR',function(req,res){
-    res.status(200).send("Hola");
-  });
-  
-app.post('/ENVIARCORDENADAS', function(req, res){
-  map.set(
-	req.body.id,'"latitud":'+req.body.latitud+','
-	+'"longitud": '+req.body.longitud+','
-	+'"velocidad": '+req.body.velocidad);
-  res.status(200).send('Exito');
+app.get('/OBTENERCORDENADAS/:id?', function (req, res, next) {
+    var response = "";
+    if (req.params.id) {
+        response = map.get(req.params.id);
+    } else {
+        response = "NO_PARAMS";
+    }
+    res.status(200).send(response);
 });
 
-app.post('/ELIMINARCORDENADAS', function(req, res){
-	map.set(req.params.id,'eliminar');
-	map.delete(req.params.id);
-	res.status(200).send('Exito');
+app.get('/OBTENERCORDENADAS2', function (req, res, next) {
+    var response = "";
+    var val = map.values();
+    var key = map.keys();
+    for (var i = 0; i < map.size; i++) {
+        response = response + '"' + key[i] + '":{' + val[i] + '}';
+        if (i + 1 < map.size) {
+            response = response + ',';
+        }
+    }
+    res.status(200).send('{' + response + '}');
+});
+
+app.get('/PROBAR', function (req, res) {
+    res.status(200).send("Hola 2");
+});
+
+app.post('/ENVIARCORDENADAS', function (req, res) {
+    map.set(
+            req.body.id, '"latitud":' + req.body.latitud + ','
+            + '"longitud": ' + req.body.longitud + ','
+            + '"velocidad": ' + req.body.velocidad);
+    tmap.set(req.body.id,'1');
+    res.status(200).send('Exito');
+});
+
+app.post('/ELIMINARCORDENADAS', function (req, res) {
+    console.log('eliminar' + req.body.id);
+    map.delete(req.body.id);
+    tmap.delete(req.body.id);
+    res.status(200).send('Exito');
 });
 
 //app.listen(3000);
-var server = app.listen((process.env.PORT || 5000), function(){
-  console.log('listening on *:5000');
+var server = app.listen((process.env.PORT || 5000), function () {
+    console.log('listening on *:5000');
 });
 //tonsquemike
